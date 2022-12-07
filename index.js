d16004ebda8f0f31c3e97977a684b3296bf78a07
Taylor Cummings <tdcummings5@gmail.com>
2022-12-06 19:39:31 -0600

    commit tree a87e0e2b17b1db695d9af5734ad35b82e9eae6a0

---
+++ index.js
@@ -0,0 +1,29 @@
+function shout(string) {
+  return  string.toUpperCase();
+}
+function whisper(string) {
+    return (string.toLowerCase())
+}
+function logShout(string) {
+    console.log(string.toUpperCase())
+    
+}
+function logWhisper(string) {
+    console.log(string.toLowerCase())
+}
+function sayHiToHeadphonedRoommate(string) {
+    // if (condition1) {
+    //     //  block of code to be executed if condition1 is true
+    //   } else if (condition2) {
+    //     //  block of code to be executed if the condition1 is false and condition2 is true
+    //   } else {
+    //     //  block of code to be executed if the condition1 is false and condition2 is false
+    //   }
+    if (string == string.toLowerCase()) {
+        return "I can't hear you!"
+    }
+}
+function sayHiToHeadphonedRoommate(string) {
+    
+}
+   
